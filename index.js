3aef035dc7337fa041a1beccb7b429b10a55cc65
aaestrada <v-aaestrada@expediagroup.com>
2021-11-04 21:13:46 -0700

    hapi-pino and hapi-pretty configuration

--- index.js
+++ index.js
@@ -1,43 +1,59 @@
-'use strict';
-
+'use strict'
+// see docs https://github.com/pinojs/pino/blob/master/docs/pretty.md *prod
+// see docs https://github.com/pinojs/pino-pretty *dev
 const Hapi = require('@hapi/hapi');
+const Pino = require('hapi-pino');
 const getResponse = [
-    {string: 'string1', number: 1, boolean: true},
-    {string: 'string2', number: 2, boolean: false},
+  {string: 'string1', number: 1, boolean: true},
+  {string: 'string2', number: 2, boolean: false},
 ];
 
-const init = async () => {
+async function start () {
+  // Create a server with a host and port
+  const server = Hapi.server({
+    host: 'localhost',
+    port: 3000,
+    debug: false, // disable Hapi debug console logging
+  })
 
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
-    });
+  // Add the route
+  server.route({
+    method: 'GET',
+    path: '/items',
+    handler: async function (request, h) {
+      return h.response(getResponse);
+    }
+  })
 
-    server.route({
-        method: 'GET',
-        path: '/items',
-        handler: function (request, h) {
-            return h.response(getResponse);
-        }
-    });
-
-    await server.register({
-        plugin: require('hapi-pino'),
+  await server.register({
+    plugin: Pino,
+    options: {
+      logPayload: true,
+      mergeHapiLogData: true,
+      ignorePaths: ['/alive.txt', '/private'],
+      ignoreFunc: (options, request) => request.path.startsWith('/private'),
+      transport: {
+        target: 'pino-pretty',
         options: {
-          prettyPrint: process.env.NODE_ENV !== 'production',
-          // Redact Authorization headers, see https://getpino.io/#/docs/redaction
-          redact: ['req.headers.authorization']
+          "colorize": true,
+          "minimumLevel": "info",
+          "levelFirst": true,
+          "messageFormat": true,
+          "timestampKey": "time",
+          "translateTime": true,
+          "ignore": "/items",
+          "singleLine": true,
+          "destination": 1
         }
-      });
+      }
+    }
+  });
+  await server.start();
+  server.log(['info'], `server running: ${server.info.uri}/items`);
+  return server
+}
 
-    await server.start();
-    console.log('Server running on %s', `${server.info.uri}/items`);
-};
-
-process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
-});
-
-init();
+start().catch((err) => {
+  console.log(err)
+  process.exit(1)
+})
